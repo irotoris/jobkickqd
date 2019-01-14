984fa815e8c66cd53cfb4098851018bfe5ac5ef5
irotoris <shiroto00@yahoo.co.jp>
2019-01-14 18:05:41 +0900

    change test

--- job/job_test.go
+++ job/job_test.go
@@ -11,8 +11,8 @@ func TestExecute(t *testing.T) {
 	jobID := "testJob"
 	cmdString := "echo \"command success test, env is ${ENV}.\""
 	envs := []string{"ENV=test"}
-	attr := make(map[string]string)
-	j := NewJob(jobID, cmdString, envs, attr)
+	timeoutDuration := 60 * time.Second
+	j := NewJob(jobID, cmdString, envs, timeoutDuration)
 
 	err := j.Execute(ctx)
 	if err != nil {
@@ -26,8 +26,8 @@ func TestKill(t *testing.T) {
 	jobID := "testJob"
 	cmdString := "sleep 1"
 	var envs []string
-	attr := make(map[string]string)
-	j := NewJob(jobID, cmdString, envs, attr)
+	timeoutDuration := 60 * time.Second
+	j := NewJob(jobID, cmdString, envs, timeoutDuration)
 
 	go j.Execute(ctx)
 
