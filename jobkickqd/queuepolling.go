eda7de6ebe86ab4fd8f9cf6b94db08aaf7582847
irotoris <shiroto00@yahoo.co.jp>
2019-01-19 22:08:37 +0900

    subscribe queue

--- jobkickqd/queuepolling.go
+++ jobkickqd/queuepolling.go
@@ -1,1 +1,50 @@
 package jobkickqd
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	"fmt"
+	"sync"
+)
+
+type JobQueueExecutor interface {
+	Run()
+	Stop()
+}
+
+type PubSubJobQueue struct {
+	projectID string
+	topicName string
+	subscriptionName string
+	pubsubClient pubsub.Client
+	subscription pubsub.Subscription
+}
+
+func NewPubSubJobQueueExecutor(ctx context.Context, projectID, topicName, subscriptionName string) (*PubSubJobQueue, error) {
+	qd := new(PubSubJobQueue)
+	qd.projectID = projectID
+	qd.topicName = topicName
+	qd.subscriptionName = subscriptionName
+	pubsubClient, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+	qd.pubsubClient = *pubsubClient
+	sub := pubsubClient.Subscription(subscriptionName)
+	qd.subscription = *sub
+	return qd ,nil
+}
+
+func (qd *PubSubJobQueue)Run(ctx, cctx context.Context ) error {
+	var mu sync.Mutex
+	err := qd.subscription.Receive(cctx, func(ctx context.Context, m *pubsub.Message) {
+		fmt.Println(string(m.Data), m.Attributes)
+		m.Ack()
+		mu.Lock()
+		defer mu.Unlock()
+	})
+	if err != context.Canceled {
+		return err
+	}
+	return nil
+}
--- jobkickqd/queuepolling_test.go
+++ jobkickqd/queuepolling_test.go
@@ -1,1 +1,49 @@
 package jobkickqd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestPubSubJobQueueSubscribe(t *testing.T) {
+
+	ctx := context.Background()
+	projectID, ok := os.LookupEnv("projectID")
+	if !ok {
+		t.Error("projectID is required.")
+	}
+
+	topicName, ok := os.LookupEnv("topicName")
+	if !ok {
+		t.Error("topicName is required.")
+	}
+
+	subscriptionName, ok := os.LookupEnv("subscriptionName")
+	if !ok {
+		t.Error("subscriptionName is required.")
+	}
+
+	qd, err := NewPubSubJobQueueExecutor(ctx, projectID, topicName, subscriptionName)
+	if err != nil {
+		fmt.Println("err", err)
+		t.Error("NewPubSubJobQueue is failed.")
+	}
+
+	cctx, cancel := context.WithCancel(ctx)
+
+	go func(queue PubSubJobQueue){
+		qd.Run(ctx, cctx)
+	}(*qd)
+
+	time.Sleep(5 * time.Second)
+	cancel()
+
+	if err != nil {
+		fmt.Println("err", err)
+		t.Error("PubSubJobQueueSubscribe() is failed.")
+	}
+
+}
