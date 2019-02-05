082055cfb96c2650f31efbe5e4fe9ba4e842f4d7
irotoris <shiroto00@yahoo.co.jp>
2019-02-06 01:57:49 +0900

    parse job parameters

--- cmd/submit.go
+++ cmd/submit.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/irotoris/jobkickqd/jobkickqd"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"io/ioutil"
 	"os"
-	"time"
+	"strings"
 )
 
 var jobTopicName string
@@ -16,7 +18,7 @@ var jobID string
 var command string
 var environmentString string
 var retry int
-var timeout time.Duration
+var timeoutInt int
 
 // submitCmd represents the submit command
 var submitCmd = &cobra.Command{
@@ -24,10 +26,38 @@ var submitCmd = &cobra.Command{
 	Short: "Submit a job command to a job queue.",
 	Long:  `Submit a job command to a job queue.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		data, err := ioutil.ReadFile(jobConfigFile)
-		if err != nil {
-			logrus.Errorf("%s:Cannot open jobConfigFile:%s", err, jobConfigFile)
-			os.Exit(1)
+		var d []byte
+		if jobConfigFile != "" {
+			data, err := ioutil.ReadFile(jobConfigFile)
+			if err != nil {
+				logrus.Errorf("%s:Cannot open jobConfigFile:%s", err, jobConfigFile)
+				os.Exit(1)
+			}
+			d = data
+		} else {
+			validParamFlag := true
+			if jobID == "" {
+				logrus.Errorf("--jobID is required if no --jobConfigFile.")
+				validParamFlag = false
+			}
+			if command == "" {
+				logrus.Errorf("--command is required if no --jobConfigFile.")
+				validParamFlag = false
+			}
+			if !validParamFlag {
+				os.Exit(1)
+			}
+			var envList []string
+			if environmentString != "" {
+				envList = strings.Split(environmentString, ",")
+			}
+			jobMessage := jobkickqd.DefaultJobMessage{JobID:jobID,Command:command, Environment:envList, Timeout:timeoutInt, Retry:retry}
+			data, err := json.Marshal(jobMessage)
+			if err != nil {
+				logrus.Errorf("JSON Marshal error in parse parameters:", err)
+				os.Exit(1)
+			}
+			d = data
 		}
 
 		// publish a job
@@ -41,7 +71,8 @@ var submitCmd = &cobra.Command{
 		attribute := map[string]string{
 			"app": "jobkickqd",
 		}
-		err = kickq.Write(ctx, string(data), attribute)
+		fmt.Printf(string(d))
+		err = kickq.Write(ctx, string(d), attribute)
 		if err != nil {
 			logrus.Errorf("%s", err)
 			os.Exit(1)
@@ -58,5 +89,5 @@ func init() {
 	submitCmd.PersistentFlags().StringVar(&command, "command", "", "command")
 	submitCmd.PersistentFlags().StringVar(&environmentString, "environment", "", "environment")
 	submitCmd.PersistentFlags().IntVar(&retry, "retry", 0, "retry count of command")
-	submitCmd.PersistentFlags().IntVar(&timeout, "timeout", 0, "timeout of command")
+	submitCmd.PersistentFlags().IntVar(&timeoutInt, "timeout", 0, "timeout of command")
 }
